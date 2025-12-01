39b24e5bca92b0873c633baa842690487e80644a
or-firefox <or.firefox@gmail.com>
2025-12-01 13:48:45 +0300

    fix low energies ER

--- utils/utils.py
+++ utils/utils.py
@@ -14,9 +14,12 @@ def get_azimov_error(OFF_1day,T_off,T_on):
 def get_signal_data(table_data,spectrum,T_on):
     signal = np.zeros(45)
     for T,sp in zip(spectrum['T[keV]'], spectrum['spectrum']):
-        if T>=0.2:
+        if T>=0.:
             str_T = int(np.round(T*1e3))/1e3
-            signal = signal+T_on*table_data[str_T]*sp
+            try:
+                signal = signal+T_on*table_data[str_T]*sp
+            except:
+                print (f'No data for {str_T} keV')
     return signal
 
 def get_chi2(x, mu, error):
